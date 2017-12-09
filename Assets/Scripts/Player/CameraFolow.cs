70a0a8bf8715298a3d190f2a1bdca5ee29f494d0
KiKu25 <kristersleinerts@gmail.com>
2017-12-09 21:52:16 +0200

    Daudz kas

--- Assets/Scripts/Player/CameraFolow.cs
+++ Assets/Scripts/Player/CameraFolow.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class CameraFolow : MonoBehaviour {
 
+    public float yOffset = 0;
+    public float zOffset = 0;
+
     GameObject player;
 
 	// Use this for initialization
 	void Start () {
         player = GameObject.FindGameObjectWithTag("Player");
+
+        if (yOffset == 0)
+            yOffset = 7;
+        if (zOffset == 0)
+            zOffset = 5;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Camera.main.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 7,player.transform.position.z - 5);
+        Camera.main.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + yOffset,player.transform.position.z - zOffset);
 	}
 }
